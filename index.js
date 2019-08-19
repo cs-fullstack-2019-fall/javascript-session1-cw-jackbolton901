a7fc39f95d90bb12834410c1e070e6355fe4226e
jackbolton901 <bltnjck@gmail.com>
2019-08-19 16:09:43 -0500

    Im lost on functions and loops. dont kno where to put it or which ones to use.

---
+++ index.js
@@ -0,0 +1,14 @@
+let myArray = ['Marvin','Ferris','Brynn','Damian','Whitney','Leila','Frances','Keith','Cara','Ainsley','Adrian','Adam','Iona','Akeem','Scarlett','Murphy','Angelica','Georgia','Calvin','Amy','Merrill','Gloria','Charissa','Colorado','Inga','Eden','Carla','Hedley','Thaddeus','Maggie','Kimberly','Ezra','Kane','Candace','Cade','Dante','Scott','Philip','Dieter','Kristen','Nerea','Ivor','Stacey','Tanner','Judith','Emery','Lionel','Josiah','Wesley','Anne','Lane','Kameko','Colleen','Travis','Abdul','Scarlet','Quail','Larissa','Palmer','Irene','Cherokee','Rajah','Maxine','Yen','Bruce','Matthew','Ivan','Georgia','Charissa','Thaddeus','Jack','Dean','Florence','Madonna','Dennis','Zeus','Destiny','Maia','Mara','Florence','Anika','Brenda','Jocelyn','Zia','Kiona','Lars','Molly','Gregory','Felix','William','Dahlia','Gil', 'Byron','Daria','Nevada','Claudia','Zelenia','Nathan','Judah','Sheila'];
+class FacebookUser
+{
+    constructor(name)
+    {
+        this.name= name
+    }
+
+}
+let facebookArray =[]
+function pusher()
+{
+    facebookArray.push()
+}
